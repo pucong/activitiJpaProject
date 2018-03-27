408106226e5aec76ca5d8294cb038abdff302607
pucong <906886111@qq.com>
2018-03-27 21:31:22 +0800

    Initial commit

---
+++ src/main/java/com/pc/activiti/activitijpaproject/ActivitiJpaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.pc.activiti.activitijpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiJpaProjectApplication.class, args);
+	}
+}
